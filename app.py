0a0aac2c7e3d38790d98b899f0e5ef5acaa1025d
khushboo5780 <khushbooshrivastav57@gmail.com>
2025-07-17 12:17:03 +0530

    Fix host and port for Railway deployment

--- app.py
+++ app.py
@@ -167,4 +167,5 @@ def chat():
         }), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # default to 5000 if PORT not set
+    app.run(host='0.0.0.0', port=port, debug=True)
